9b4990714a3106f3ae544503fbfc67e7506cad95
Reem <ralattas@live.com>
2021-06-10 07:58:18 +0000

    Implement solution

---
+++ Google/Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums = nums1 + nums2
+        nums = sorted(nums)
+        
+        print(len(nums))
+        if len(nums) % 2 != 0:
+            print("Odd => 1 value")
+            res = nums[len(nums)//2]
+        else:
+            print("Even => Avg of 2 values")
+            res = (nums[len(nums)//2] + nums[(len(nums)//2)-1]) / 2.0
+        
+        return res
