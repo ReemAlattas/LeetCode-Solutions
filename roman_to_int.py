b6baf7f4a7378816c238ee40ecee7988f0c5273d
Reem <ralattas@live.com>
2021-08-14 11:35:34 +0300

    Create roman_to_int.py

---
+++ roman_to_int.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman = {'I': 1, 'V': 5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        ans = 0
+        for i in range(len(s)-1, -1, -1):
+            num = roman[s[i]]
+            if 4*num < ans:
+                ans -= num
+            else:
+                ans += num
+        
+        return ans
+            
